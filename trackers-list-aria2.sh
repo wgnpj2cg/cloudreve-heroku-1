626ee3132d50abf281d8f7b86f5b984ea0d5c721
FuaerCN <FuaerCN@Gmail.com>
2020-05-07 22:15:02 +0800

    Update trackers-list-aria2.sh

--- trackers-list-aria2.sh
+++ trackers-list-aria2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/etc/init.d/aria2 stop
 list=`wget -qO- https://trackerslist.com/all.txt |awk NF|sed ":a;N;s/\n/,/g;ta"`
 if [ -z "`grep "bt-tracker" /root/aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/aria2/aria2.conf
@@ -9,4 +8,4 @@ else
     echo update......
 fi
 rm -rf /root/Download/*
-/etc/init.d/aria2 start
+aria2c --conf-path=/root/aria2/aria2.conf
